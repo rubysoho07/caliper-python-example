042c3b593d1fa2823d7e36978a36197fdc22ce4d
Yungon Park <hahafree12@gmail.com>
2017-10-31 10:47:16 +0900

    Add pages for tagging and submitting quiz

--- example/__init__.py
+++ example/__init__.py
@@ -5,14 +5,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def first_page():
+    # TODO: Make SessionEvent if a session doesn't exist, or make NavigationEvent.
     return 'Test page!'
 
 
 @app.route('/read')
 def reading_page():
+    # TODO: Make NavigationEvent
     return 'Reading material!'
+
+
+@app.route('/tag')
+def tag_page():
+    # TODO: Make AnnotationEvent
+    return 'Tagged'
 
 
 @app.route('/quiz')
 def quiz_page():
+    # TODO: Make AssessmentEvent
     return 'Quiz Start!'
+
+
+@app.route('/quiz_submit')
+def quiz_submit():
+    # TODO: Make GradeEvent
+    return 'Quiz submitted and graded!'
