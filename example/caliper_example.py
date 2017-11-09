453fc51868050a5cea443f6f50fcc83e8eb1203f
Yungon Park <hahafree12@gmail.com>
2017-11-09 13:39:04 -0800

    Change indentation

--- example/caliper_example.py
+++ example/caliper_example.py
@@ -72,6 +72,7 @@ def reading_page():
     action = getattr(navigation_event, 'action')
 
     sensor.send(navigation_event)
+
     return render_template('reading.html',
                            event=event_type,
                            action=action,
@@ -83,40 +84,37 @@ def tag_page():
     """
     Generate tag and send AnnotationEvent
     """
-    if request.method == 'POST':
-        tags = list(tag for tag in request.form['tags'].split(','))
+    tags = list(tag for tag in request.form['tags'].split(','))
 
-        # Tag
-        generated_tag = entities.TagAnnotation(
-            id=BASE_URI + "/user/193828/course/2017/ssed514/document/11/tag/1",
-            annotator=example_user,
-            annotated=reading_material,
-            tags=tags
-        )
+    # Tag
+    generated_tag = entities.TagAnnotation(
+        id=BASE_URI + "/user/193828/course/2017/ssed514/document/11/tag/1",
+        annotator=example_user,
+        annotated=reading_material,
+        tags=tags
+    )
 
-        # Create and send AnnotationEvent
-        annotation_event = events.AnnotationEvent(
-            actor=example_user,
-            action=BASIC_EVENT_ACTIONS['TAGGED'],
-            object=reading_material,
-            generated=generated_tag,
-            eventTime=datetime.datetime.now().isoformat(),
-            membership=example_membership,
-            session=session
-        )
+    # Create and send AnnotationEvent
+    annotation_event = events.AnnotationEvent(
+        actor=example_user,
+        action=BASIC_EVENT_ACTIONS['TAGGED'],
+        object=reading_material,
+        generated=generated_tag,
+        eventTime=datetime.datetime.now().isoformat(),
+        membership=example_membership,
+        session=session
+    )
 
-        event_type = getattr(annotation_event, 'type')
-        action = getattr(annotation_event, 'action')
+    event_type = getattr(annotation_event, 'type')
+    action = getattr(annotation_event, 'action')
 
-        sensor.send(annotation_event)
+    sensor.send(annotation_event)
 
-        return render_template('reading.html',
-                               event=event_type,
-                               action=action,
-                               event_data=annotation_event.as_json(thin_context=True, thin_props=True),
-                               tags=tags)
-
-    return render_template('reading.html')
+    return render_template('reading.html',
+                           event=event_type,
+                           action=action,
+                           event_data=annotation_event.as_json(thin_context=True, thin_props=True),
+                           tags=tags)
 
 
 @app.route('/quiz')
@@ -137,6 +135,7 @@ def quiz_page():
     action = getattr(assessment_event, 'action')
 
     sensor.send(assessment_event)
+
     return render_template('quiz.html',
                            event=event_type,
                            action=action,
@@ -148,62 +147,59 @@ def quiz_submit():
     """
     Create and send AssessmentEvent(Submitted) and GradeEvent(Graded)
     """
-    if request.method == 'POST':
-        answer = int(request.form['test-question'])
+    answer = int(request.form['test-question'])
 
-        if answer == 2:
-            score_given = 10.0
-        else:
-            score_given = 0.0
+    if answer == 2:
+        score_given = 10.0
+    else:
+        score_given = 0.0
 
-        assessment_event = events.AssessmentEvent(
-            actor=example_user,
-            action=BASIC_EVENT_ACTIONS['SUBMITTED'],
-            object=assessment,
-            eventTime=datetime.datetime.now().isoformat()
-        )
+    assessment_event = events.AssessmentEvent(
+        actor=example_user,
+        action=BASIC_EVENT_ACTIONS['SUBMITTED'],
+        object=assessment,
+        eventTime=datetime.datetime.now().isoformat()
+    )
 
-        attempt = entities.Attempt(
-            id=BASE_URI+"/course/2017/ssed514/assessment/1/attempt/1",
-            assignee=example_user,
-            assignable=assessment,
-            count=1
-        )
+    attempt = entities.Attempt(
+        id=BASE_URI+"/course/2017/ssed514/assessment/1/attempt/1",
+        assignee=example_user,
+        assignable=assessment,
+        count=1
+    )
 
-        score = entities.Score(
-            id=BASE_URI + "/course/2017/ssed514/assessment/1/attempt/1",
-            attempt=attempt,
-            maxScore=15.0,
-            scoreGiven=score_given,
-            dateCreated=datetime.datetime.now().isoformat()
-        )
+    score = entities.Score(
+        id=BASE_URI + "/course/2017/ssed514/assessment/1/attempt/1",
+        attempt=attempt,
+        maxScore=15.0,
+        scoreGiven=score_given,
+        dateCreated=datetime.datetime.now().isoformat()
+    )
 
-        grade_event = events.GradeEvent(
-            actor=ed_app,
-            action=BASIC_EVENT_ACTIONS['GRADED'],
-            object=attempt,
-            generated=score,
-            eventTime=datetime.datetime.now().isoformat(),
-            membership=example_membership,
-            session=example_session
-        )
+    grade_event = events.GradeEvent(
+        actor=ed_app,
+        action=BASIC_EVENT_ACTIONS['GRADED'],
+        object=attempt,
+        generated=score,
+        eventTime=datetime.datetime.now().isoformat(),
+        membership=example_membership,
+        session=example_session
+    )
 
-        # Test to send multiple events in list.
-        sensor.send([assessment_event, grade_event])
+    # Test to send multiple events in list.
+    sensor.send([assessment_event, grade_event])
 
-        event_type = getattr(assessment_event, 'type') + " & " + getattr(grade_event, 'type')
-        action = getattr(assessment_event, 'action') + " & " + getattr(grade_event, 'action')
+    event_type = getattr(assessment_event, 'type') + " & " + getattr(grade_event, 'type')
+    action = getattr(assessment_event, 'action') + " & " + getattr(grade_event, 'action')
 
-        # To show AssessmentEvent & GradeEvent in JSON format.
-        event_list = [
-            assessment_event.as_dict(thin_context=True, thin_props=True),
-            grade_event.as_dict(thin_props=True, thin_context=True)
-        ]
+    # To show AssessmentEvent & GradeEvent in JSON format.
+    event_list = [
+        assessment_event.as_dict(thin_context=True, thin_props=True),
+        grade_event.as_dict(thin_props=True, thin_context=True)
+    ]
 
-        return render_template('quiz.html',
-                               event=event_type,
-                               action=action,
-                               event_data=json.dumps(event_list),
-                               answer=answer)
-
-    return render_template('quiz.html')
+    return render_template('quiz.html',
+                           event=event_type,
+                           action=action,
+                           event_data=json.dumps(event_list),
+                           answer=answer)
